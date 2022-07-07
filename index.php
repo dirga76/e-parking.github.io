6f8e5075cb27cc8da8eb8bf2dcb2c94ff3bdb487
dirga76 <87862021+dirga76@users.noreply.github.com>
2022-07-07 20:46:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sistem informasi E parking</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="/js/script.js"></script>
+</head>
+
+<body>
+    <form class="box" action="proseslogin.php" method="post">
+        <h1>Login E Parking</h1>
+        <input type="text" name="username" placeholder="enter Username" id="username" autocomplete="off"/>
+        <input type="password" name="password" placeholder="enter Password" id="password"autocomplete="off"/>
+        <input type="submit" name="" value="login" onclick="validate()" />
+        <a href="../html/lp.html"></a>
+    </form>
+</body>
+
+</html>
---
+++ proseslogin.php
@@ -0,0 +1,61 @@
+<?php 
+// mengaktifkan session pada php
+session_start();
+ 
+// menghubungkan php dengan koneksi database
+include_once("config/helper.php");
+include_once("config/config.php");
+include_once 'config/function.php';
+
+// menangkap data yang dikirim dari form login
+$username = $_POST['username'];
+$password = $_POST['password'];
+ 
+ 
+// menyeleksi data user dengan username dan password yang sesuai
+$login = mysqli_query($config,"SELECT * from user where username='$username' and password='$password'");
+// menghitung jumlah data yang ditemukan
+$cek = mysqli_num_rows($login);
+ 
+// cek apakah username dan password di temukan pada database
+if($cek > 0){
+ 
+	$data = mysqli_fetch_assoc($login);
+ 
+	// cek jika user login sebagai admin
+	if($data['level']=="Admin"){
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "Admin";
+		// alihkan ke halaman dashboard admin
+		header("location:admin/index.php");
+ 
+	// cek jika user login sebagai petugas
+	}else if($data['level']=="Petugas"){
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "Petugas";
+		// alihkan ke halaman dashboard petugas
+		header("location:petugas/index.php");
+ 
+	// cek jika user login sebagai owner
+	}else if($data['level']=="Owner"){
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "Owner";
+		// alihkan ke halaman dashboard owner
+		header("location:owner/index.php");
+ 
+	}else{
+ 
+		// alihkan ke halaman login kembali
+		header("location:index.php?pesan=gagal");
+	}	
+}else{
+	header("location:index.php?pesan=gagal");
+}
+ 
+?>
+
+
+$
